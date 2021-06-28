c1fe45c62d4c2d4b7ab69ff6b6baeeaadd0067b3
Artem <47538574+Penultimo@users.noreply.github.com>
2021-06-28 17:45:07 +0300

    Update ARRAY-isItHere-FindMatch.kt

--- src/main/kotlin/ARRAY-isItHere-FindMatch.kt
+++ src/main/kotlin/ARRAY-isItHere-FindMatch.kt
@@ -5,3 +5,16 @@ fun main (args: Array<String>){
     if (arr[arr.lastIndex] in arr.dropLast(1)) counter = true
     if (counter) println("YES") else println("NO")
 }
+
+/*
+fun main() {
+    val size = readLine()!!.toInt()
+    val array = IntArray(size)
+
+    for (i in 0..array.lastIndex) {
+        array[i] = readLine()!!.toInt()
+    }
+
+    println(if (readLine()!!.toInt() in array) "YES" else "NO")
+}
+*/
